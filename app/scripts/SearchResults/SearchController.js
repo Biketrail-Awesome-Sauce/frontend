aeeb0a2b031ebae6d05f73c3085a698fd91f17f2
boydjohnson <lt359mn38>
2015-12-21 21:31:37 -0600

    Initial commit

---
+++ app/scripts/SearchResults/SearchController.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('mnbikewaysMap').controller('searchCtlr', ['$scope', '$http', function($scope, $http){}]);
---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mnbikewaysApp
+ * @description
+ * # mnbikewaysApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mnbikewaysMap', [
+    'ui.bootstrap.typeahead'
+  ]);
